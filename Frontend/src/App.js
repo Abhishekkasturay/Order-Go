bc1213f6957a44799896eb6ced1a9522b3e1d5cc
Abhishekk <136403845+Abhishekkasturay@users.noreply.github.com>
2025-02-10 02:57:36 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Header from "./Components/Header";
 import UserContext from "./utils/UserContext";
-import PaymentPage from "./Components/Payment"; // Import Payment Page
 
 const App = () => {
   const [userName, setUserName] = useState("");
@@ -10,7 +9,7 @@ const App = () => {
   // Simulate an API call to get user data
   useEffect(() => {
     const data = {
-      name: "Guest",
+      name: "",
     };
     setUserName(data.name);
   }, []);
@@ -19,15 +18,10 @@ const App = () => {
     <UserContext.Provider
       value={{ loggedInUser: userName, setLoggedInUser: setUserName }}
     >
-      <Router>
-        <div className="App">
-          <Header />
-          <Routes>
-            <Route path="/payment" element={<PaymentPage />} />
-            {/* Add other routes here */}
-          </Routes>
-        </div>
-      </Router>
+      <div className="App">
+        <Header />
+        <Outlet />
+      </div>
     </UserContext.Provider>
   );
 };
